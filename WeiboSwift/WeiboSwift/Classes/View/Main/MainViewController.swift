2648922f4ed3e6dc4a5be0b253530ccdf5b85232
Sanonnb <huangguowu35@126.com>
2021-06-16 18:19:12 +0800

    uitabbar的调整,设置tabbar上选中文字的大小和图片的缩进

--- WeiboSwift/WeiboSwift/Classes/View/Main/MainViewController.swift
+++ WeiboSwift/WeiboSwift/Classes/View/Main/MainViewController.swift
@@ -9,21 +9,22 @@
 import UIKit
 
 class MainViewController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         
         self.view.backgroundColor = UIColor.brownColor()
+        
         //添加子控制器
-         tabBar.tintColor = UIColor.orangeColor()
+        tabBar.tintColor = UIColor.orangeColor()
         
         addChildViewController(HomeTableViewController(), title: "首页", imageName: "tabbar_home")
         addChildViewController(MessageTableViewController(), title: "消息", imageName: "tabbar_message_center")
         addChildViewController(DiscoverTableViewController(), title: "发现", imageName:  "tabbar_discover")
         addChildViewController(ProfileTableViewController(), title: "我", imageName: "tabbar_profile")
     }
-
+    
     
     
 //MARK: -  ①添加子控制器
@@ -40,18 +41,41 @@ class MainViewController: UITabBarController {
         addChildViewController(nav)
     }
 
+///Users/yp-apple/Documents/代码/2016年7月代码/WeiboSwift/WeiboSwift/WeiboSwift/Assets.xcassets/TabBar/allproducts.imageset/allproducts@2x.png
    
     
 //MARK: - ②抽象出一个方法添加多个控制器
     
     
     private func addChildViewController(vc: UIViewController,title:String,imageName:String) {
-        
+      
+        //设置标题
         vc.title = title
+        
+        //设置背景图片
         vc.tabBarItem.image = UIImage(named: imageName)
         vc.tabBarItem.selectedImage = UIImage(named: "\(imageName)_selected")
+        
+        //设置选中文字的颜色
+        let textColorAttr = [
+            NSForegroundColorAttributeName: UIColor.orangeColor()
+        ]
+        vc.tabBarItem.setTitleTextAttributes(textColorAttr, forState: UIControlState.Selected)
+        
+        
+        
+        //设置字体大小属性
+        let textFontAttr = [
+            NSFontAttributeName :UIFont.systemFontOfSize(13)
+        ]
+        
+        vc.tabBarItem.setTitleTextAttributes(textFontAttr, forState: UIControlState.Normal)
+        
+        
         let nav = UINavigationController(rootViewController:vc)
         addChildViewController(nav)
+        
+        
     }
     
     
